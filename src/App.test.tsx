8d6365648e92edbfe8bfe8b9bd73723518846a88
Ben <manglebot@users.noreply.github.com>
2023-12-11 09:45:30 +0000

    fix:add AAA comments to test files. complete activity 1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders form title', () => {
-	render(<App />);
-	const formTitle = screen.getByText(
-		/W-12-M :- APPLICATION TO SPARE PLANET FROM DESTRUCTION/i
-	);
-	expect(formTitle).toBeInTheDocument();
+test("renders form title", () => {
+  // Arrange
+  render(<App />);
+  // Act
+  const formTitle = screen.getByText(
+    /W-12-M :- APPLICATION TO SPARE PLANET FROM DESTRUCTION/i
+  );
+  // Assert
+  expect(formTitle).toBeInTheDocument();
 });
--- src/components/W12MHeader.test.tsx
+++ src/components/W12MHeader.test.tsx
@@ -1,10 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import W12MHeader from './W12MHeader';
+import { render, screen } from "@testing-library/react";
+import W12MHeader from "./W12MHeader";
 
-test('renders header text', () => {
-	render(<W12MHeader />);
-	const someHeaderText = screen.getByText(
-		/Each species may only submit ONE W-12-M form./i
-	);
-	expect(someHeaderText).toBeInTheDocument();
+test("renders header text", () => {
+  // Arrange
+  render(<W12MHeader />);
+  // Act
+  const someHeaderText = screen.getByText(
+    /Each species may only submit ONE W-12-M form./i
+  );
+  // Assert
+  expect(someHeaderText).toBeInTheDocument();
 });
