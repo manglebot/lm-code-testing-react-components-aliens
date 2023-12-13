04f51c138a550f1049595e8314ecfbda5cb18964
Ben <manglebot@users.noreply.github.com>
2023-12-13 21:54:36 +0000

    fix:update speciesName and test to use useState

--- src/components/speciesName.test.tsx
+++ src/components/speciesName.test.tsx
@@ -1,35 +1,30 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { SpeciesName } from "./speciesName";
 
 describe("Chek the Species Name label and input", () => {
-  test("find the label and input tags and their classes", () => {
+  test("find the label and input tags and their classes, and simulate input change", () => {
     // Arrange
-    const speciesName = "Dolphin";
-    const onChangeSpeciesName = (e: React.ChangeEvent<HTMLInputElement>) => {};
+    // let inputValue = "Human";
+    const initialSpeciesName = "Human";
 
     // Act
-    render(
-      <SpeciesName
-        speciesName={speciesName}
-        onChangeSpeciesName={onChangeSpeciesName}
-      />
-    );
+    render(<SpeciesName initialSpeciesName={initialSpeciesName} />);
 
     // Act
     const labelElement = document.querySelector("label");
-    const inputElement = document.querySelector("input")!;
-    const inputElementValue = inputElement.value;
-
     const labelClass = document.querySelector("label.form__label");
+    const inputElement = document.querySelector("input")!;
     const inputClass = document.querySelector("input.form__input");
+
+    fireEvent.change(inputElement, { target: { value: "Dolphin" } });
 
     // Assert
     expect(labelElement).toBeInTheDocument();
-    expect(inputElement).toBeInTheDocument();
     expect(labelClass).toBeInTheDocument();
+    expect(inputElement).toBeInTheDocument();
     expect(inputClass).toBeInTheDocument();
 
     expect(labelElement).toHaveTextContent("Species Name:");
-    expect(inputElementValue).toBe("Dolphin");
+    expect(inputElement.value).toBe("Dolphin");
   });
 });
--- src/components/speciesName.tsx
+++ src/components/speciesName.tsx
@@ -1,21 +1,29 @@
+import React, { useState } from "react";
+
 interface SpeciesNameProps {
-  speciesName: string;
-  onChangeSpeciesName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  initialSpeciesName: string;
 }
 
 export const SpeciesName: React.FC<SpeciesNameProps> = ({
-  speciesName,
-  onChangeSpeciesName,
-}) => (
-  <>
-    <label htmlFor="speciesName" className="form__label">
-      Species Name:
-    </label>
-    <input
-      className="form__input"
-      id="speciesName"
-      value={speciesName}
-      onChange={onChangeSpeciesName}
-    />
-  </>
-);
+  initialSpeciesName,
+}) => {
+  const [speciesName, setSpeciesName] = useState(initialSpeciesName);
+
+  const onChangeSpeciesName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSpeciesName(e.target.value);
+  };
+
+  return (
+    <>
+      <label htmlFor="speciesName" className="form__label">
+        Species Name:
+      </label>
+      <input
+        className="form__input"
+        id="speciesName"
+        value={speciesName}
+        onChange={onChangeSpeciesName}
+      />
+    </>
+  );
+};
