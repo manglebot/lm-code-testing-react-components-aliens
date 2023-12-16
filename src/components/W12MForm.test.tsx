4800e5336c55097e235f7eca6f7b947515d677a6
Ben <manglebot@users.noreply.github.com>
2023-12-16 12:40:53 +0000

    feat:add Planet Name validation tests

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,5 +1,8 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import W12MForm from "./W12MForm";
+// import ValidateSpeciesName from "../validate/validate_species_name";
+
+import "@testing-library/jest-dom/extend-expect";
 
 describe("W12M Form render and submit tests", () => {
   test("renders form element", () => {
@@ -27,6 +30,20 @@ describe("W12M Form render and submit tests", () => {
   });
 
   describe("Tests for Species Name", () => {
+    let mockValidateSpeciesName: jest.Mock;
+
+    beforeEach(() => {
+      mockValidateSpeciesName = jest.fn();
+      jest.mock("../validate/validate_species_name", () => ({
+        __esModule: true,
+        default: mockValidateSpeciesName,
+      }));
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
     test("SpeciesName input updates state correctly", () => {
       // Arrange
       render(<W12MForm />);
@@ -41,9 +58,74 @@ describe("W12M Form render and submit tests", () => {
       // Assert
       expect(speciesNameInput).toHaveValue("Human");
     });
+
+    test("Form validation displays SpeciesName error message", () => {
+      // Arrange
+      mockValidateSpeciesName.mockReturnValue(
+        "Species Name must be between 3 and 23 characters"
+      );
+
+      // Act
+      render(<W12MForm />);
+
+      const submitButton = document.querySelector('button[type="submit"]')!;
+      fireEvent.click(submitButton);
+
+      // Assert
+      const speciesNameInput = document.getElementById("speciesName")!;
+      const errorMessage = speciesNameInput.nextElementSibling;
+      expect(errorMessage).toHaveTextContent(
+        "Species Name must be between 3 and 23 characters"
+      );
+    });
+
+    test("Form validation hides SpeciesName error message", () => {
+      // Arrange
+      mockValidateSpeciesName.mockReturnValueOnce(
+        "Species Name must be between 3 and 23 characters"
+      );
+      mockValidateSpeciesName.mockReturnValueOnce("");
+
+      // Act
+      render(<W12MForm />);
+
+      const submitButton = document.querySelector('button[type="submit"]')!;
+      fireEvent.click(submitButton);
+
+      // Assert (initial error message present)
+      const speciesNameInput = document.getElementById("speciesName")!;
+      const initialErrorMessage = speciesNameInput.nextElementSibling;
+
+      // console.log("initialErrorMessage");
+      // console.log(initialErrorMessage?.textContent);
+
+      expect(initialErrorMessage).toHaveTextContent(
+        "Species Name must be between 3 and 23 characters"
+      );
+
+      fireEvent.change(speciesNameInput, { target: { value: "Dolphin" } });
+
+      // Assert
+      const errorMessageAfterChange = speciesNameInput.nextElementSibling!;
+      expect(errorMessageAfterChange).not.toBeInTheDocument;
+    });
   });
 
   describe("Tests for Planet Name", () => {
+    let mockValidatePlanetName: jest.Mock;
+
+    beforeEach(() => {
+      mockValidatePlanetName = jest.fn();
+      jest.mock("../validate/validate_planet_name", () => ({
+        __esModule: true,
+        default: mockValidatePlanetName,
+      }));
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
     test("PlanetName input updates state correctly", () => {
       // Arrange
       render(<W12MForm />);
@@ -53,11 +135,58 @@ describe("W12M Form render and submit tests", () => {
         name: /Planet Name:/i,
       }) as HTMLInputElement;
 
-      // expect(planetNameInput.value).toBe("");
       fireEvent.change(planetNameInput, { target: { value: "Earth" } });
 
       // Assert
       expect(planetNameInput.value).toBe("Earth");
+    });
+
+    test("Form validation displays Planet Name error message", () => {
+      // Arrange
+      mockValidatePlanetName.mockReturnValue(
+        "Planet Name must be between 2 and 49 characters."
+      );
+
+      // Act
+      render(<W12MForm />);
+
+      const submitButton = document.querySelector('button[type="submit"]')!;
+      fireEvent.click(submitButton);
+
+      // Assert
+      const planetNameInput = document.getElementById("planetName")!;
+      const errorMessage = planetNameInput.nextElementSibling;
+      expect(errorMessage).toHaveTextContent(
+        "Planet Name must be between 2 and 49 characters."
+      );
+    });
+
+    test("Form validation hides Planet Name error message", () => {
+      // Arrange
+      mockValidatePlanetName.mockReturnValueOnce(
+        "Planet Name must be between 2 and 49 characters."
+      );
+      mockValidatePlanetName.mockReturnValueOnce("");
+
+      // Act
+      render(<W12MForm />);
+
+      const submitButton = document.querySelector('button[type="submit"]')!;
+      fireEvent.click(submitButton);
+
+      // Assert (initial error message present)
+      const planetNameInput = document.getElementById("planetName")!;
+      const initialErrorMessage = planetNameInput.nextElementSibling;
+
+      expect(initialErrorMessage).toHaveTextContent(
+        "Planet Name must be between 2 and 49 characters."
+      );
+
+      fireEvent.change(planetNameInput, { target: { value: "Saturn" } });
+
+      // Assert
+      const errorMessageAfterChange = planetNameInput.nextElementSibling!;
+      expect(errorMessageAfterChange).not.toBeInTheDocument;
     });
   });
 
